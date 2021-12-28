0b09ea5e6bd34d23ea1620b277c739d6c89d1a99
psddevops <ppreddy2711@gmail.com>
2021-12-28 07:28:40 +0530

    added maven project

---
+++ src/main/java/retail/HelloMaven.java
@@ -0,0 +1,7 @@
+// Your First Program
+package retail;
+public class HelloMaven {
+    public static void main(String[] args) {
+        System.out.println("Hello welcome to Maven ....."); 
+    }
+}
---
+++ src/main/java/retail/HelloWorld.java
@@ -0,0 +1,6 @@
+package retail;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
